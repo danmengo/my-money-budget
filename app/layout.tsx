703d1c9953e287d88003da942d731c485f779041
danmengo <98555319+danmengo@users.noreply.github.com>
2026-09-26 00:10:09 -0700

    Enable system light and dark themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import ThemeProvider from "./theme-provider";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://budget.danmengo.com"),
@@ -27,5 +28,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en"><body className="antialiased">{children}</body></html>;
+  return <html lang="en" suppressHydrationWarning><body className="antialiased"><ThemeProvider>{children}</ThemeProvider></body></html>;
 }
